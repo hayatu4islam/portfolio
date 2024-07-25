5706fa2521d5ecb7cc37e5196b370a5405865bb7
Hayat <hayatu4islam@yahoo.com>
2024-07-26 00:24:44 +0100

    begin commit

---
+++ src/pages/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='menu-container'>
+      <div className="menu">
+        <a href="#home">Home</a>
+      </div>
+      <div className="menu">
+        <a href="#about">About</a>
+      </div>
+      <div className="menu">
+        <a href="#portfolio">Portfolio</a>
+      </div>
+      <div className="menu">
+        <a href="#github">GitHub</a>
+      </div>
+      <div className="menu">
+        <a href="#linkedin">LinkedIn</a>
+      </div>
+      <div className="menu">
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Menu
